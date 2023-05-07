fe6f17ffdbbb1343d7896bd8a89ca7d71d2dc256
bshong12 <bshong3593@dgu.ac.kr>
2023-05-08 00:56:43 +0900

    [23-05-04]

--- 2023-1r-webprogramming/src/App.jsx
+++ 2023-1r-webprogramming/src/App.jsx
@@ -4,7 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // count의 값을 바꾸고 싶으면 setCount를 실행
+  // const [count, setCount] = useState(0) //useState함수의 결과는 리스트
+  const [row, setRow] = useState([]);
+
+  function loadbtn(){
+  if(row.length === 0)
+  fetch("http://openapi.seoul.go.kr:8088/636f65566e6273683737536e467763/json/RealtimeCityAir/1/25/").then(
+    function(res2) {
+      res2.json().then(function(res3) {
+        setRow(res3.RealtimeCityAir.row);
+      })
+    }
+  )
+  }
+
+  // async await 사용 힘듬
+  // const res = await fetch("http://openapi.seoul.go.kr:8088/636f65566e6273683737536e467763/json/RealtimeCityAir/1/25/");
+  // const res2 = await res.json();
+
+  console.log(row)
 
   return (
     <>
@@ -17,7 +36,33 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+
+      <button onClick={loadbtn}>Loading</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>PM10</th>
+            <th>O3</th>
+            <th>상태</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {row.map(function(obj) 
+          {
+            return <tr>
+              <td>{obj.MSRSTE_NM}</td>
+              <td>{obj.PM10}</td>
+              <td>{obj.O3}</td>
+              <td>{obj.IDEX_NM}</td>
+            </tr>
+          })
+          }
+        </tbody>
+      </table>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +70,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
           안녕하세요 한글
         </p>
-      </div>
+      </div> */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- 2023-1r-webprogramming/src/main.jsx
+++ 2023-1r-webprogramming/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(   // root라는 태그를 찾아 그 안에 App 넣음
   <React.StrictMode>
     <App />
   </React.StrictMode>,
